7a692935a11898a899506e988190d2b9de85e0b0
lakpa-finju <connectedtofinju1@gmail.com>
2023-09-16 05:05:19 -0400

    changed the model.

--- app.py
+++ app.py
@@ -3,12 +3,18 @@
 # This model is for detecting food in the image.
 # Use a pipeline as a high-level helper
 from transformers import pipeline
+import os
+import openai
+
+openai.organization = "org-5Z0c3Uk1VG7t3TsczN6M4FCi"
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key_path ="./key.txt" 
+print(openai.api_key)
+print(openai.Model.list())
+
 
 pipe = pipeline("image-classification", model="microsoft/resnet-50")
 result = pipe("./man-holding-banana.jpeg")
 print(result[0]['label'])
 
 
-pipe = pipeline("text-generation", model="tiiuae/falcon-7b-instruct", trust_remote_code=True)
-result = pipe("what can I make with apple, orange, and potato?")
-print("this is the query result.")
